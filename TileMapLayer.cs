e14cc16701f1961c05b65536a60eed80e9a27578
Carlos Vializ <cvializ@vt.edu>
2024-12-08 21:10:26 -0500

    It's really working

--- TileMapLayer.cs
+++ TileMapLayer.cs
@@ -1,8 +1,54 @@
 using Godot;
 using System;
 
+
+public partial class CircleNode2D : Godot.Node2D
+{
+	public override void _Draw()
+	{
+		this.DrawCircle(new Vector2(0f, 0f), 3f, new Color(1, 1, 1, 1));
+	}
+}
+
 public partial class TileMapLayer : Godot.TileMapLayer
 {	
+	private double _NormalizedAtan2(double y, double x)
+	{
+		var angle = -Math.Atan2(y, x);
+		return (angle > 0 ? angle : angle + Math.PI * 2);
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (!(@event is InputEventMouseButton mouseEvent))
+		{
+			return;
+		}
+		
+		if (!mouseEvent.Pressed) {
+			return;
+		}
+		
+		var globalCoords = mouseEvent.Position;
+		var localCoords = this.ToLocal(globalCoords);
+		var mapCoords = this.LocalToMap(localCoords);
+				
+		var digits = this._GetCellDigitsNode(mapCoords);
+		var digitsLocalCoords = digits.ToLocal(globalCoords);
+		
+		var length = Math.Sqrt(digitsLocalCoords.X * digitsLocalCoords.X + digitsLocalCoords.Y * digitsLocalCoords.Y);
+		
+		if (length < 10f) {
+			GD.Print("vertex", 0);
+			return;
+		}
+		
+		var angle = this._NormalizedAtan2(digitsLocalCoords[1], digitsLocalCoords[0]);
+		var vertex = Math.Floor((angle / (Math.PI / 3)) + Math.PI / 6) % 6 + 1;
+		
+		GD.Print("vertex", vertex);
+	}
+	
 	private Node2D _CreateLabel(String text)
 	{
 		var container = new Node2D();
@@ -12,46 +58,59 @@ public partial class TileMapLayer : Godot.TileMapLayer
 		
 		label.SetTheme(theme);
 		label.SetText(text);
-		label.SetPosition(new Vector2(-size[0] / 2, -size[1] / 2));
+		
+		label.SetPosition(new Vector2(-3, -7));
 		label.SetZIndex(1);
-				
+		
 		container.AddChild(label);
 		
 		return container;
 	}
-	//
-	//private void _AddLabelAtCoords(String text, Vector2[] coords)
-	//{
-		//var label = this._CreateLabel(text);
-		//label.SetPosition(coords);
-		//return label;
-	//}
 	
-	// Called when the node enters the scene tree for the first time.
+	private Node2D _GetCellDigitsNode(Vector2 mapCoords)
+	{
+		var cells = GetNode("cells");
+		return (Node2D)cells.GetNode($"{mapCoords}");
+	}
+	
 	public override void _Ready()
 	{
+		var cells = new Node2D();
+		cells.Name = "cells";
+		
 		var tileSize = this.GetTileSet().GetTileSize();
 		var usedCells = this.GetUsedCells();
+		
 		for (int i = 0; i < usedCells.Count; i++) 
 		{			
-			var cellCoordsLocal = this.MapToLocal(usedCells[i]);
+			var digits = new CircleNode2D();
 			
-			var centerLabel = this._CreateLabel($"{i}");
-			centerLabel.SetPosition(cellCoordsLocal);
-			this.AddChild(centerLabel);
+			var mapCoords = usedCells[i];
+			var cellCoordsLocal = this.MapToLocal(mapCoords);
+			
+			digits.Name = $"{mapCoords}";
+			digits.SetPosition(cellCoordsLocal);
+			
+			var centerLabel = this._CreateLabel($"{0}");
+			digits.AddChild(centerLabel);
 			
 			for (int j = 0; j < 6; j++) {
 				var label = this._CreateLabel($"{j + 1}");
 				
 				var angle = 2 * Math.PI / 6 * j;
 				
-				var adjacent = tileSize[0] / 2 * Math.Cos(-angle);
-				var opposite = tileSize[1] / 2 * Math.Sin(-angle);
+				var adjacent = (tileSize[0] / 2.5) * Math.Cos(-angle);
+				var opposite = (tileSize[1] / 2.5) * Math.Sin(-angle);
 				
-				label.SetPosition(new Vector2(cellCoordsLocal.X + (float)adjacent, cellCoordsLocal.Y + (float)opposite));
-				this.AddChild(label);
+				label.SetPosition(new Vector2((float)adjacent, (float)opposite));
+				digits.AddChild(label);
 			}
+			
+			cells.AddChild(digits);
 		}
+		
+		this.AddChild(cells);
+		GD.Print(cells.Position);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
