aac2df149c21f6a76bd10cc360f984d02fc803a7
Carlos Vializ <cvializ@vt.edu>
2024-12-19 22:06:00 -0500

    Cleanup

--- CoordinateUtils.cs
+++ CoordinateUtils.cs
@@ -6,7 +6,7 @@ public partial class CoordinateUtils : Godot.Node
 {
     public static Vector4I WUnitVector = new Vector4I(0, 0, 0, 1);
     
-    public static Vector4I RotateW(Vector4I vector, int max = 3)
+    public static Vector4I RotateW(Vector4I vector)
     {
         var xyzVector = new Vector4I(1, 1, 1, 0) * vector;
         var wVector = WUnitVector * vector;
@@ -16,10 +16,11 @@ public partial class CoordinateUtils : Godot.Node
             return xyzVector + (wVector % 3);
         }
         
-        // 0, -1, -2, -3, ... => 0 2 1 0 2 1 0 ... 
+        // -1, -2, -3, ... => 0 2 1 0 2 1 0
+        // -3, -3, -4, ... =>
+        // 0, -1, -2, 0, -1, ... =>
+        // 2, 1, 0, 2, 1, 0
         var rotatedWVector = ((wVector - 2 * WUnitVector) % 3) + 2 * WUnitVector;
-        
-        //GD.Print("rotatedWVector", rotatedWVector, xyzVector);
         
         return xyzVector + rotatedWVector;
     }
@@ -44,9 +45,7 @@ public partial class CoordinateUtils : Godot.Node
     public static Vector4I Direction4W = -Direction4E;
     public static Vector4I CarryW = -CarryVector4;
     
-    
     // Vector2
-
     public static Vector2I Direction2NW = new Vector2I(0, -1);
     public static Vector2I Direction2SE = -Direction2NW;
     public static Vector2I Direction2SW = new Vector2I(-1, 1);
@@ -54,11 +53,8 @@ public partial class CoordinateUtils : Godot.Node
     public static Vector2I Direction2E = new Vector2I(1, 0);
     public static Vector2I Direction2W = -Direction2E;
     
-    
-    
     public static Vector4I TranslateVector(Vector4I initial, int delta, Vector4I direction)
     {
-        var normalizedDelta = Math.Abs(delta);
         Vector4I carry;
         switch (direction)
         {
@@ -91,24 +87,13 @@ public partial class CoordinateUtils : Godot.Node
                 carry = new Vector4I(-999, -999, -999, -999);
                 break;
         }
+        var normalizedDelta = Math.Abs(delta);
         var normalizedCarry = Math.Sign(delta) * carry;
         
-        //GD.Print("TranslateVector");
         int carryCount = Math.Abs(GetCarryCount(initial.W, Math.Sign(direction.W) * delta));
-        //GD.Print("carryCount", carryCount);
-        //GD.Print("initial", initial);
-        //GD.Print("direction", direction);
-        //GD.Print("delta", delta);
-        
         var carryVector = normalizedCarry * carryCount;
-        //GD.Print("carryVector", carryVector);
         
-        //GD.Print($"(initial + direction * delta) + carryVector {RotateW(initial + (direction * delta) + carryVector)}");
-        var result = RotateW(initial + (direction * delta) + carryVector);
-        
-        //GD.Print("result: ", result);
-        
-        return result;
+        return RotateW(initial + (direction * delta) + carryVector);
     }
     
     public static Vector2I TranslateVector(Vector2I initial, int delta, Vector2I direction)
@@ -126,17 +111,7 @@ public partial class CoordinateUtils : Godot.Node
         Vector4I initialVector = new Vector4I(0, 0, 0, 0);
         Vector4I vectorTranslatedE = TranslateVector(initialVector, q, Direction4E);
         Vector4I vectorTranslatedEThenSE = TranslateVector(vectorTranslatedE, r, Direction4SE);
-        
-        //
-        //Vector4I unitsE = TranslateVector(new Vector4I(0, 0, 0, 0), q, Direction4E);
-        //Vector4I unitsSE = TranslateVector(new Vector4I(0, 0, 0, 0), r, Direction4SE);
-        //
-        //int carryCount = GetCarryCount(unitsSE.W, unitsE.W);
-        //
-        //GD.Print($"unitsE {unitsE} unitsSE {unitsSE}");
-        //// I have to add the translations to get 
-        //return RotateW(unitsE + unitsSE + CarryVector4 * carryCount);
-        
+            
         return vectorTranslatedEThenSE;
     }
     
@@ -155,37 +130,9 @@ public partial class CoordinateUtils : Godot.Node
     
     public override void _Ready()
     {
-        //GD.Print("WOW", Vector2IToVector4I(new Vector2I(2, 1)));
-        
-        //GD.Print("(0, 0, 0, 0) NW 1", TranslateVector(new Vector4I(0, 0, 0, 0), 1, Direction4NW));
-        //GD.Print("(0, 0, 0, 0) NW 2", TranslateVector(new Vector4I(0, 0, 0, 0), 2, Direction4NW));
-        //GD.Print("(0, 0, 0, 0) NW 3", TranslateVector(new Vector4I(0, 0, 0, 0), 3, Direction4NW));
-        //GD.Print("(0, 0, 0, 0) NW 4", TranslateVector(new Vector4I(0, 0, 0, 0), 4, Direction4NW));
-        //
-        //GD.Print("(0, 0, 0, 0) SE 1", TranslateVector(new Vector4I(0, 0, 0, 0), 1, Direction4SE));
-        //GD.Print("(0, 0, 0, 0) SE 2", TranslateVector(new Vector4I(0, 0, 0, 0), 2, Direction4SE));
-        //GD.Print("(0, 0, 0, 0) SE 3", TranslateVector(new Vector4I(0, 0, 0, 0), 3, Direction4SE));
-        //GD.Print("(0, 0, 0, 2) SE 1", TranslateVector(new Vector4I(0, 0, 0, 2), 1, Direction4SE));
-        //GD.Print("(0, 0, 0, 0) SE 4", TranslateVector(new Vector4I(0, 0, 0, 0), 4, Direction4SE));
-        //GD.Print("(0, 0, 0, 1)", TranslateVector(new Vector4I(0, 0, 0, 0), 1, Direction4NE));
-        
         GD.Print("Vector2IToVector4I (0, -1) to ", Vector2IToVector4I(new Vector2I(0, -1)));
         GD.Print("Vector2IToVector4I (1, 0) to ", Vector2IToVector4I(new Vector2I(1, 0)));
         GD.Print("Vector2IToVector4I (1, 1) to ", Vector2IToVector4I(new Vector2I(1, 1)));
-        //GD.Print(TranslateVector(new Vector2I(1, 0), 1, Direction2SE));
-        //GD.Print("XXX", TranslateVector(new Vector4I(0, 0, 0, 0), 1, Direction4E));
-        
-        for (int w = 0; w < 10; w++)
-        {
-            GD.Print($"RotateW {-5 + w} ", RotateW(new Vector4I(0, 0, 0, -5 + w)));
-            for (int d = 0; d < 10; d++)
-            {
-                //var carry = GetCarryCount(w, -5 + d);
-                //GD.Print($"w + d = carry {w} + {-5 + d} = {carry}");
-            }
-            GD.Print("");
-        }
-        //
-        //GD.Print(new Vector4I(1, 1, 1, 0) * new Vector4I(5, -6, 1, 3));
+        GD.Print("Vector2IToVector4I (2, 1) to ", Vector2IToVector4I(new Vector2I(2, 1)));
     }
 }
